cf60e9bf779e33cb720e4155d0a48eb4d6f361f0
masaki takahashi <bje35149@gmail.com>
2023-08-26 10:47:02 +0900

    ADD：pre-commit ディレクトリ移動コマンド 修正

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
   )
