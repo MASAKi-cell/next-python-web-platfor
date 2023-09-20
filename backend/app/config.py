ccb039f5985d120d40921917e97a95e0268dace7
masaki takahashi <bje35149@gmail.com>
2023-09-20 10:33:46 +0900

    ADD：config.pyの設定追加

---
+++ backend/app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+
+load_dotenv(verbose=True)  # ファイルが見つからない場合は警告を出力する
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)  # 相対パスの取得
+openai_api_ket = os.environ.get('OPENAI_API_KEY')
