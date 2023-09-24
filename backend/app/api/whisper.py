bba7d9068015bcbe245f065a7e27ea63d60b6aef
masaki takahashi <bje35149@gmail.com>
2023-09-24 22:57:33 +0900

    ADD： mypyの追加

--- backend/app/api/whisper.py
+++ backend/app/api/whisper.py
@@ -4,14 +4,14 @@ from config import openai_api_key
 
 
 # 音声をWhisper APIで文字に変換する関数
-def voice_to_text():
+def voice_to_text() -> str:
+    openai.api_key = openai_api_key  # Api Keyのセット
     dotenv_path = join(
         "/Users/takahashi_masaki/Desktop/Whisper-chatGTP-Api/backend/asano.wav"
     )
-    openai.api_key = openai_api_key  # Api Keyのセット
     audio_file = open(dotenv_path, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    print(transcript)
+    return transcript["text"]
 
 
 voice_to_text()
