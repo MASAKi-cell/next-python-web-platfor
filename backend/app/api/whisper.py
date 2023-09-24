7d3719db2b0f59ea4484d452a3c010fb305dc9da
masaki takahashi <bje35149@gmail.com>
2023-09-24 21:55:43 +0900

    ADD：音声をWhisper APIで文字に変換する関数の追加

---
+++ backend/app/api/whisper.py
@@ -0,0 +1,23 @@
+from os.path import join
+import openai
+
+import sys
+from os.path import dirname, abspath
+
+# sys.pathに追加
+sys.path.append(dirname(dirname(abspath(__file__))))
+from config import openai_api_key
+
+
+# 音声をWhisper APIで文字に変換する関数
+def voice_to_text():
+    dotenv_path = join(
+        "/Users/takahashi_masaki/Desktop/Whisper-chatGTP-Api/backend/asano.wav"
+    )
+    openai.api_key = openai_api_key  # Api Keyのセット
+    audio_file = open(dotenv_path, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
+
+
+voice_to_text()
--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ from os.path import join, dirname
 
 
 load_dotenv(verbose=True)  # ファイルが見つからない場合は警告を出力する
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)  # 相対パスの取得
-openai_api_ket = os.environ.get('OPENAI_API_KEY')
+openai_api_key = os.environ.get("OPENAI_API_KEY")
