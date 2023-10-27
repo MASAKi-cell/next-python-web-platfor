77cb587c2c96bff77747ef25fb05403ffab6d276
masaki takahashi <bje35149@gmail.com>
2023-10-27 20:46:36 +0900

    ADD：chat-gtpによる返答文の生成処理を追加

--- backend/app/api/whisper.py
+++ backend/app/api/whisper.py
@@ -13,16 +13,23 @@ def voice_to_text() -> str:
         audio_file = open(dotenv_path, "rb")
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
         return transcript["text"]
-    except openai.OpenAIError:
+    except openai.OpenAIError as error:
+        print(f"Error in the audio: {error}")
         return "Error in processing the audio"
 
 
 # chat-gtpによる返答文の生成
 def think_response(text_message: str) -> str:
     try:
+        openai.api_key = openai_api_key  # Api Keyのセット
         chat_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=text_message
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": text_message},
+            ],
         )
         return chat_response["choices"][0]["message"]["content"]
-    except openai.OpenAIError:
+    except openai.OpenAIError as error:
+        print(f"Error in chat-gtp: {error}")
         return "Error in processing the chat-gtp"
