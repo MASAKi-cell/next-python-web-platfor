881b42658e60b7ccf22d5cc395e6ea89c19b2d87
masaki takahashi <bje35149@gmail.com>
2023-11-28 10:47:50 +0900

    ADD： 音声出力処理コードの修正

--- backend/app/api/voicevox.py
+++ backend/app/api/voicevox.py
@@ -1,18 +1,21 @@
-from time import sleep
 import json
 import requests
 from config import voicevox_path
+
+# 例外処理
 from utils.exceptions import ExceptionsError, HttpCode
+
+# 音声出力
+from time import sleep
 import pyaudio
 import wave
 import io
 
-speaker = "speaker"
 one = 1
 
 
 # 音声合成処理
-def post_audio(text: str) -> bytes | None:
+def post_audio(text: str) -> str | None:
     try:
         param: dict = {"text": text, "speaker": one}
         res = requests.post(f"{voicevox_path}/audio_query", params=param)
@@ -36,7 +39,7 @@ def post_audio(text: str) -> bytes | None:
 # 音声をバイトデータに変換
 def post_synthesis(audio_query_res: str) -> bytes | None:
     try:
-        params = {speaker: one}
+        params = {"speaker": one}
         headers = {"content-type": "application/json"}
         audio_query_res_json = json.dumps(audio_query_res)
         res = requests.post(
@@ -61,21 +64,26 @@ def post_synthesis(audio_query_res: str) -> bytes | None:
     return None
 
 
-def play_wav(wav_file: bytes | None):
+# 音声出力
+def play_wav(wav_file: bytes | None) -> None:
+    FORMAT = pyaudio.paInt16  # 量子ビット
+    CHANNELS = 1  # ch数
+    RATE = 23000  # サンプリング周波数
+    CHUNK = 2**10  # バッファサイズ
     if wav_file is not None:
         wr: wave.Wave_read = wave.open(io.BytesIO(wav_file))
     p = pyaudio.PyAudio()
     stream = p.open(
-        format=p.get_format_from_width(wr.getsampwidth()),
-        channels=wr.getnchannels(),
-        rate=wr.getframerate(),
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
         output=True,
     )
-    chunk = 1024
-    data = wr.readframes(chunk)
+
+    data = wr.readframes(CHUNK)
     while data:
         stream.write(data)
-        data = wr.readframes(chunk)
+        data = wr.readframes(CHUNK)
     sleep(0.5)
     stream.close()
     p.terminate()
@@ -86,6 +94,3 @@ def text_to_voice(text: str):
     if res is not None:
         synthesis = post_synthesis(res)
         play_wav(synthesis)
-
-
-text_to_voice("こんにちは")
