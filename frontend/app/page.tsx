b0c01824faf960aab2cbc85937e87ba7f880abc3
masaki takahashi <bje35149@gmail.com>
2023-12-04 10:40:52 +0900

    ADD：recorderの削除

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,9 @@
-import Recorder from './usemedia'
 import styles from './page.module.css'
 
 export default function Home(): JSX.Element {
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        <Recorder />
-      </div>
+      <div className={styles.grid}></div>
     </main>
   )
 }
